82ec0bb98d1985ad83c71451a0a76d217efce79f
Mendel Greenberg <chabad360@gmail.com>
2024-07-16 14:32:12 -0400

    Expose the event class to allow creating a new one

--- web/src/auto.ts
+++ web/src/auto.ts
@@ -1,12 +1,16 @@
 import { Live } from "./live";
 import { Hooks } from "./interop";
+import {LiveEvent as LE} from "./event";
 
 declare global {
     interface Window {
         Hooks: Hooks;
         Live: Live;
     }
+    export const LiveEvent: typeof LE;
+    export type LiveEvent = LE;
 }
+(window as any).LiveEvent = LE;
 
 document.addEventListener("DOMContentLoaded", (_) => {
     if (window.Live !== undefined) {
